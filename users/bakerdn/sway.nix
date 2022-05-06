7bdba3869d625024012fd73089af6350ca832ae0
Daniel Baker <daniel.baker@tweag.io>
2022-05-06 15:53:56 -0700

    added a keybinding.

--- users/bakerdn/sway.nix
+++ users/bakerdn/sway.nix
@@ -1,9 +1,15 @@
-{ config, pkgs, ... }:
-
+{ pkgs, config, ... }:
+let
+  mod = config.wayland.windowManager.sway.config.modifier;
+in
 {
   wayland.windowManager.sway.enable = true;
   wayland.windowManager.sway.wrapperFeatures.gtk = true;
   wayland.windowManager.sway.config.modifier = "Mod4";
   wayland.windowManager.sway.config.terminal = "${pkgs.kitty}/bin/kitty";
-  wayland.windowManager.sway.config.bars = [{ command = "${config.programs.waybar.package}/bin/waybar"; }]; 
+  wayland.windowManager.sway.config.bars = [{ command = "${config.programs.waybar.package}/bin/waybar"; }];
+
+  wayland.windowManager.sway.config.keybindings = {
+    "${mod}+Space" = "exec wofi --show drun";
+  };
 }
