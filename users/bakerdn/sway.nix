dbe98246f6395d6721762ce4d14981b9716d381e
Daniel Baker <daniel.baker@tweag.io>
2022-05-06 19:26:40 -0700

    added keybindings to open wofi.

--- users/bakerdn/sway.nix
+++ users/bakerdn/sway.nix
@@ -1,7 +1,5 @@
-{ pkgs, config, ... }:
-let
-  mod = config.wayland.windowManager.sway.config.modifier;
-in
+{ pkgs, lib, config, ... }:
+
 {
   wayland.windowManager.sway.enable = true;
   wayland.windowManager.sway.wrapperFeatures.gtk = true;
@@ -9,7 +7,12 @@ in
   wayland.windowManager.sway.config.terminal = "${pkgs.kitty}/bin/kitty";
   wayland.windowManager.sway.config.bars = [{ command = "${config.programs.waybar.package}/bin/waybar"; }];
 
-  wayland.windowManager.sway.config.keybindings = {
-    "${mod}+Space" = "exec wofi --show drun";
+  wayland.windowManager.sway.config.keybindings =
+  let
+    mod = config.wayland.windowManager.sway.config.modifier;
+  in
+  lib.mkOptionDefault
+  {
+    "${mod}+d" = "exec wofi --show drun";
   };
 }
