85128e59dd6148ca4f3213bed9b584548ba6e25f
Daniel Baker <daniel.baker@tweag.io>
2022-05-19 20:06:29 -0700

    Updated bash configuration.

---
+++ users/bakerdn/bash.nix
@@ -0,0 +1,44 @@
+{ ... }:
+
+{
+  programs.bash.enable = true;
+  programs.bash.historyControl =
+  [
+    "erasedups"
+    "ignoredups"
+    "ignorespace"
+  ];
+  programs.bash.historyFileSize = 100000;
+  programs.bash.historySize = 10000;
+  programs.bash.initExtra =
+  ''
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/tweag-github
+  '';
+  programs.bash.shellAliases =
+  {
+    lse = "ls -Fho";
+    lsa = "lse -A";
+  };
+  programs.bash.shellOptions =
+  [
+    # Append to history file rather than replacing it.
+    "histappend"
+
+    # Check the window size after each command and, if necessary, update the
+    # values of LINES and COLUMNS.
+    "checkwinsize"
+
+    # Extended globbing.
+    "extglob"
+    "globstar"
+
+    # Warn if closing shell with running jobs.
+    "checkjobs"
+
+    # Do not fix cd commands or cd into a directory if a command was given that
+    # matches a directory name.
+    "-autocd"
+    "-cdspell"
+  ];
+}
