3c2a1d1aabed2d2e95af4e66d15c3d34a1a55900
Daniel Baker <daniel.baker@tweag.io>
2022-09-29 09:54:27 -0700

    added network manager.

--- system/configuration.nix
+++ system/configuration.nix
@@ -34,7 +34,7 @@
     initialPassword = "password";
     extraGroups = [
       "wheel"
-      "networkManager"
+      "networkmanager"
     ];
   };
 
@@ -50,11 +50,12 @@
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
   # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp0s20f0u3.useDHCP = true;
-  networking.interfaces.enp0s31f6.useDHCP = true;
+  # networking.useDHCP = false;
+  # networking.interfaces.enp0s20f0u3.useDHCP = true;
+  # networking.interfaces.enp0s31f6.useDHCP = true;
   # networking.interfaces.enp45s0u2u1u1.useDHCP = true;
-  networking.interfaces.wlp0s20f3.useDHCP = true;
+  # networking.interfaces.wlp0s20f3.useDHCP = true;
+  networking.networkmanager.enable = true;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
